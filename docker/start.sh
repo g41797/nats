f97489873908c2521a15dc92fa2a103823deb296
g41797 <g41797@gmail.com>
2025-01-05 12:18:12 +0200

    Add docker environment

---
+++ docker/start.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+export QPORT=4222
+
+if ! [ -f docker/docker-compose.yml ]; then
+  export DOCOMPOSE=./docker-compose.yml
+else
+  export DOCOMPOSE=docker/docker-compose.yml
+fi
+
+docker compose -f $DOCOMPOSE up -d
+
+echo Waiting for open port $QPORT
+
+for (( ; ; ))
+do
+    sudo netstat --tcp --listening --programs --numeric|grep -o $QPORT|wc -l >/tmp/openedports
+    OPENED=$(< /tmp/openedports)
+    if [ $OPENED -gt 0 ];
+    then
+        break
+    fi
+
+    echo -n .
+    sleep 1
+done
+
+echo 'ok'
+echo ''
+
+date
+sudo netstat --tcp --listening --programs --numeric|grep $QPORT
+echo ''
+
+
