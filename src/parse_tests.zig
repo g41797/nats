80b4d6d7a3743f8c988bd33e2e0a46e6825e2f07
g41797 <g41797@gmail.com>
2025-01-09 08:55:41 +0200

    Add skeleton of parse APIs

--- src/parse_tests.zig
+++ src/parse_tests.zig
@@ -9,33 +9,33 @@ const ReturnedError = err.ReturnedError;
 const parse = @import("parse.zig");
 
 test "parse errors" {
-    try testing.expectError(ReturnedError.Unknown, parse.parseSize(""));
-    try testing.expectError(ReturnedError.Unknown, parse.parseSize("TEXT"));
-    try testing.expectError(std.fmt.ParseIntError.InvalidCharacter, parse.parseSize("TEXT TEXT TEXT"));
+    try testing.expectError(ReturnedError.Unknown, parse.cut_tail_size(""));
+    try testing.expectError(ReturnedError.Unknown, parse.cut_tail_size("TEXT"));
+    try testing.expectError(std.fmt.ParseIntError.InvalidCharacter, parse.cut_tail_size("TEXT TEXT TEXT"));
 }
 
 // RESERVED <id> <bytes>\r\n
 // FOUND <id> <bytes>\r\n
 // KICKED <count>\r\n
 // OK <bytes>\r\n
-test "parseSize" {
-    var result = try parse.parseSize("RESERVED 101 234\r\n");
-    try testing.expectEqual(std.mem.eql(u8, "RESERVED 101", result[0]), true);
-    try testing.expectEqual(234, result[1]);
+test "cut_tail_size" {
+    var result = try parse.cut_tail_size("RESERVED 101 234\r\n");
+    try testing.expectEqual(std.mem.eql(u8, "RESERVED 101", result.shrinked), true);
+    try testing.expectEqual(234, result.size);
 
-    result = try parse.parseSize(result[0]);
-    try testing.expectEqual(std.mem.eql(u8, "RESERVED", result[0]), true);
-    try testing.expectEqual(101, result[1]);
+    result = try parse.cut_tail_size(result.shrinked);
+    try testing.expectEqual(std.mem.eql(u8, "RESERVED", result.shrinked), true);
+    try testing.expectEqual(101, result.size);
 
-    result = try parse.parseSize("FOUND 101 234");
-    try testing.expectEqual(std.mem.eql(u8, "FOUND 101", result[0]), true);
-    try testing.expectEqual(234, result[1]);
+    result = try parse.cut_tail_size("FOUND 101 234");
+    try testing.expectEqual(std.mem.eql(u8, "FOUND 101", result.shrinked), true);
+    try testing.expectEqual(234, result.size);
 
-    result = try parse.parseSize("KICKED 101");
-    try testing.expectEqual(std.mem.eql(u8, "KICKED", result[0]), true);
-    try testing.expectEqual(101, result[1]);
+    result = try parse.cut_tail_size("KICKED 101");
+    try testing.expectEqual(std.mem.eql(u8, "KICKED", result.shrinked), true);
+    try testing.expectEqual(101, result.size);
 
-    result = try parse.parseSize("OK 101\n");
-    try testing.expectEqual(std.mem.eql(u8, "OK", result[0]), true);
-    try testing.expectEqual(101, result[1]);
+    result = try parse.cut_tail_size("OK 101\n");
+    try testing.expectEqual(std.mem.eql(u8, "OK", result.shrinked), true);
+    try testing.expectEqual(101, result.size);
 }
