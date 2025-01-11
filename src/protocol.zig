2d8126ab774feb5665ecf747eaae84a4b7f129ec
g41797 <g41797@gmail.com>
2025-01-11 17:38:02 +0200

    Sat Jan 11 05:37:37 PM IST 2025

--- src/protocol.zig
+++ src/protocol.zig
@@ -5,99 +5,74 @@ const std = @import("std");
 const ascii = std.ascii;
 const EnumMap = std.enums.EnumMap;
 const Allocator = std.mem.Allocator;
+const Header = std.http.Header;
+const HeaderIterator = std.http.HeaderIterator;
 const err = @import("err.zig");
 const ReturnedError = err.ReturnedError;
 const parse = @import("parse.zig");
 
-pub const Appendable = struct {
-    buffer: ?[]u8 = null,
-    len: usize = undefined,
-    actual_len: usize = 0,
-    allocator: Allocator = undefined,
 
-    pub fn init(apndbl: *Appendable, allocator: Allocator, len: usize) !void {
-        apndbl.allocator = allocator;
-        try apndbl.alloc(len);
-        return;
-    }
+// ==========================================
+// Messages with payload and optional headers
+// ==========================================
 
-    pub fn reset(apndbl: *Appendable) !void {
-        if (apndbl.buffer == null) {
-            error.WasNotAllocated;
-        }
-        apndbl.actual_len = 0;
-        return;
-    }
+// =======================================
+//                  PUB [Client]
+// =======================================
+// PUB <subject> [reply-to] <#bytes>␍␊[payload]␍␊
+//
+// #bytes> - length of payload without ␍␊
+//
+// PUB FOO 11␍␊Hello NATS!␍␊
+//
+// PUB FRONT.DOOR JOKE.22 11␍␊Knock Knock␍␊
+//
+// PUB NOTIFY 0␍␊␍␊     #bytes == 0 => empty payload
+//
 
-    pub fn deinit(apndbl: *Appendable) void {
-        apndbl.free();
-    }
+// =======================================
+//                  MSG [Server]
+// =======================================
+// MSG <subject> <sid> [reply-to] <#bytes>␍␊[payload]␍␊
+//
+// #bytes> - length of payload without ␍␊
+//
+// MSG FOO.BAR 9 11␍␊Hello World␍␊
+//
+// MSG FOO.BAR 9 GREETING.34 11␍␊Hello World␍␊
 
-    pub inline fn append(apndbl: *Appendable, buff: []u8) !void {
-        if (apndbl.buffer == null) {
-            error.WasNotAllocated;
-        }
-        _ = buff;
-        return;
-    }
+// =======================================
+//                  HPUB [Client]
+// =======================================
+// HPUB SUBJECT REPLY 23 30␍␊NATS/1.0␍␊Header: X␍␊␍␊PAYLOAD␍␊
+// HPUB SUBJECT REPLY 23 23␍␊NATS/1.0␍␊Header: X␍␊␍␊␍␊
+// HPUB SUBJECT REPLY 48 55␍␊NATS/1.0␍␊Header1: X␍␊Header1: Y␍␊Header2: Z␍␊␍␊PAYLOAD␍␊
+// HPUB SUBJECT REPLY 48 48␍␊NATS/1.0␍␊Header1: X␍␊Header1: Y␍␊Header2: Z␍␊␍␊␍␊
+//
+// HPUB <SUBJ> [REPLY] <HDR_LEN> <TOT_LEN>
+// <HEADER><PAYLOAD>
+//
+// HDR_LEN includes the entire serialized header, from the start of the version string (NATS/1.0)
+// up to and including the ␍␊ before the payload
+//
+// TOT_LEN the payload length plus the HDR_LEN
 
-    pub fn fill(apndbl: *Appendable, from: []u8, len: usize) !void {
-        if (from.len < len) {
-            return error.OutOfBoundary;
-        }
+// =======================================
+//                  HMSG [Server]
+// =======================================
+// HMSG SUBJECT 1 REPLY 23 30␍␊NATS/1.0␍␊Header: X␍␊␍␊PAYLOAD␍␊
+// HMSG SUBJECT 1 REPLY 23 23␍␊NATS/1.0␍␊Header: X␍␊␍␊␍␊
+// HMSG SUBJECT 1 REPLY 48 55␍␊NATS/1.0␍␊Header1: X␍␊Header1: Y␍␊Header2: Z␍␊␍␊PAYLOAD␍␊
+// HMSG SUBJECT 1 REPLY 48 48␍␊NATS/1.0␍␊Header1: X␍␊Header1: Y␍␊Header2: Z␍␊␍␊␍␊
+//
+// HMSG <SUBJECT> <SID> [REPLY] <HDR_LEN> <TOT_LEN>
+// <PAYLOAD>
+//
+// HDR_LEN includes the entire serialized header, from the start of the version string (NATS/1.0)
+// up to and including the ␍␊ before the payload
+//
+// TOT_LEN the payload length plus the HDR_LEN
 
-        try apndbl.reset();
-        try apndbl.append(from[0..len]);
-
-        return;
-    }
-
-    pub fn alloc(apndbl: *Appendable, len: usize) !void {
-        if (apndbl.buffer == null) {
-            apndbl.len = roundlen(len);
-            apndbl.actual_len = 0;
-            apndbl.buffer = try apndbl.allocator.alloc(u8, apndbl.len);
-            return;
-        }
-
-        const rlen = roundlen(len);
-
-        if (apndbl.len >= rlen) {
-            return;
-        }
-
-        apndbl.buffer = try apndbl.allocator.realloc(apndbl.buffer[0..apndbl.actual_len], rlen);
-
-        return;
-    }
-
-    fn free(apndbl: *Appendable) void {
-        if (apndbl.buffer != null) {
-            apndbl.allocator.free(apndbl.buffer.?);
-            apndbl.buffer = null;
-            apndbl.actual_len = 0;
-            apndbl.len = 0;
-        }
-        return;
-    }
-
-    pub fn body(apndbl: *Appendable) ?[]u8 {
-        if (apndbl.buffer == null) {
-            return null;
-        }
-        if (apndbl.actual_len == 0) {
-            return null;
-        }
-        return apndbl.buffer.?[0..apndbl.actual_len];
-    }
-
-    inline fn roundlen(len: usize) usize {
-        if (len == 0) {
-            return 256;
-        }
-        return ((len / 256) + 1) * 256;
-    }
-};
 
 pub const MessageType = enum {
     UNKNOWN,
@@ -160,84 +135,34 @@ pub fn cut_message_type(line: []const u8) !struct { mt: ?MessageType, tail: []co
     return error.BadFormat;
 }
 
-pub const Header = struct {
-    name: []const u8,
-    value: []const u8,
-};
-
-// =======================================
-//                  HPUB
-// =======================================
-// HPUB SUBJECT REPLY 23 30␍␊NATS/1.0␍␊Header: X␍␊␍␊PAYLOAD␍␊
-// HPUB SUBJECT REPLY 23 23␍␊NATS/1.0␍␊Header: X␍␊␍␊␍␊
-// HPUB SUBJECT REPLY 48 55␍␊NATS/1.0␍␊Header1: X␍␊Header1: Y␍␊Header2: Z␍␊␍␊PAYLOAD␍␊
-// HPUB SUBJECT REPLY 48 48␍␊NATS/1.0␍␊Header1: X␍␊Header1: Y␍␊Header2: Z␍␊␍␊␍␊
-//
-// HPUB <SUBJ> [REPLY] <HDR_LEN> <TOT_LEN>
-// <HEADER><PAYLOAD>
-//
-// HDR_LEN includes the entire serialized header, from the start of the version string (NATS/1.0)
-// up to and including the ␍␊ before the payload
-//
-// TOT_LEN the payload length plus the HDR_LEN
-
-// =======================================
-//                  HMSG
-// =======================================
-// HMSG SUBJECT 1 REPLY 23 30␍␊NATS/1.0␍␊Header: X␍␊␍␊PAYLOAD␍␊
-// HMSG SUBJECT 1 REPLY 23 23␍␊NATS/1.0␍␊Header: X␍␊␍␊␍␊
-// HMSG SUBJECT 1 REPLY 48 55␍␊NATS/1.0␍␊Header1: X␍␊Header1: Y␍␊Header2: Z␍␊␍␊PAYLOAD␍␊
-// HMSG SUBJECT 1 REPLY 48 48␍␊NATS/1.0␍␊Header1: X␍␊Header1: Y␍␊Header2: Z␍␊␍␊␍␊
-//
-// HMSG <SUBJECT> <SID> [REPLY] <HDR_LEN> <TOT_LEN>
-// <PAYLOAD>
-//
-// HDR_LEN includes the entire serialized header, from the start of the version string (NATS/1.0)
-// up to and including the ␍␊ before the payload
-//
-// TOT_LEN the payload length plus the HDR_LEN
-
-pub const HeaderIterator = struct {
-    bytes: []const u8,
-    index: usize,
-    is_trailer: bool,
-
-    pub fn init(bytes: []const u8) HeaderIterator {
-        return .{
-            .bytes = bytes,
-            .index = std.mem.indexOfPosLinear(u8, bytes, 0, "\r\n").? + 2,
-            .is_trailer = false,
-        };
-    }
-
-    pub fn next(it: *HeaderIterator) ?Header {
-        _ = it;
-        return null;
-    }
-};
-
 pub const Headers = struct {
-    apndbl: Appendable = undefined,
+    buffer: Appendable = undefined,
 
     pub fn init(hdrs: *Headers, allocator: Allocator, len: usize) !void {
-        try hdrs.apndbl.init(allocator, len);
+        try hdrs.buffer.init(allocator, len);
         return;
     }
 
     pub fn deinit(hdrs: *Headers) void {
-        hdrs.apndbl.free();
+        hdrs.buffer.free();
     }
 
     pub fn append(hdrs: *Headers, name: []u8, value: []u8) !void {
-        try hdrs.apndbl.append(name);
-        try hdrs.apndbl.append(":");
-        try hdrs.apndbl.append(value);
-        try hdrs.apndbl.append("\r\n");
+        if (hdrs.buffer.actual_len == 0) {
+            try hdrs.buffer.append("NATS/1.0\r\n");
+        } else {
+            hdrs.buffer.shrink(1) catch void; // Remove tail "\r\n"
+        }
+        try hdrs.buffer.append(name);
+        try hdrs.buffer.append(":");
+        try hdrs.buffer.append(value);
+        try hdrs.buffer.append("\r\n");
+        try hdrs.buffer.append("\r\n"); // Add tail
         return;
     }
 
     pub fn reset(hdrs: *Headers) !void {
-        try hdrs.apndbl.reset();
+        try hdrs.buffer.reset();
         return;
     }
 
@@ -312,5 +237,100 @@ pub const MSG = struct {
             return msg.payload.body();
         }
         return null;
+    }
+};
+
+pub const Appendable = struct {
+    buffer: ?[]u8 = null,
+    actual_len: usize = 0,
+    allocator: Allocator = undefined,
+
+    pub fn init(apndbl: *Appendable, allocator: Allocator, len: usize) !void {
+        apndbl.allocator = allocator;
+        try apndbl.alloc(len);
+        return;
+    }
+
+    pub fn reset(apndbl: *Appendable) !void {
+        if (apndbl.buffer == null) {
+            error.WasNotAllocated;
+        }
+        apndbl.actual_len = 0;
+        return;
+    }
+
+    pub fn deinit(apndbl: *Appendable) void {
+        apndbl.free();
+    }
+
+    pub inline fn append(apndbl: *Appendable, buff: []u8) !void {
+        if (apndbl.buffer == null) {
+            return error.WasNotAllocated;
+        }
+        _ = buff;
+        return;
+    }
+
+    pub inline fn shrink(apndbl: *Appendable, count: usize) !void {
+        if (apndbl.buffer == null) {
+            return error.WasNotAllocated;
+        }
+        if (apndbl.actual_len < count) {
+            return error.NotEnoughMemory;
+        }
+        apndbl.actual_len -= count;
+        return;
+    }
+
+    pub fn copy(apndbl: *Appendable, from: []u8) !void {
+        try apndbl.reset();
+        try apndbl.append(from);
+
+        return;
+    }
+
+    pub fn alloc(apndbl: *Appendable, len: usize) !void {
+        if (apndbl.buffer == null) {
+            apndbl.len = roundlen(len);
+            apndbl.actual_len = 0;
+            apndbl.buffer = try apndbl.allocator.alloc(u8, apndbl.len);
+            return;
+        }
+
+        const rlen = roundlen(len);
+
+        if (apndbl.len >= rlen) {
+            return;
+        }
+
+        apndbl.buffer = try apndbl.allocator.realloc(apndbl.buffer[0..apndbl.actual_len], rlen);
+
+        return;
+    }
+
+    fn free(apndbl: *Appendable) void {
+        if (apndbl.buffer != null) {
+            apndbl.allocator.free(apndbl.buffer.?);
+            apndbl.buffer = null;
+            apndbl.actual_len = 0;
+        }
+        return;
+    }
+
+    pub fn body(apndbl: *Appendable) ?[]u8 {
+        if (apndbl.buffer == null) {
+            return null;
+        }
+        if (apndbl.actual_len == 0) {
+            return null;
+        }
+        return apndbl.buffer.?[0..apndbl.actual_len];
+    }
+
+    inline fn roundlen(len: usize) usize {
+        if (len == 0) {
+            return 256;
+        }
+        return ((len / 256) + 1) * 256;
     }
 };
