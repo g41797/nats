90fbd3410cee1a010e656daf3e80b455188d21a0
g41797 <g41797@gmail.com>
2025-05-08 10:22:31 +0300

    Try to run tests under windows

--- build.zig
+++ build.zig
@@ -102,7 +102,7 @@ pub fn build(b: *std.Build) void {
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/root_tests.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -118,7 +118,7 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    // test_step.dependOn(&run_exe_unit_tests.step);
+    // test_step.dependOn(&exe_unit_tests.step);
 }
 
 const std = @import("std");
