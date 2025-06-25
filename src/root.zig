5028b8772a15b84a9c7ff959db93ac74ac2c754b
g41797 <g41797@gmail.com>
2025-06-25 11:57:57 +0300

    Set several structs pub in order to use from another modules.

--- src/root.zig
+++ src/root.zig
@@ -1,15 +1,17 @@
 // Copyright (c) 2025 g41797
 // SPDX-License-Identifier: MIT
 
-const parse = @import("parse.zig");
+pub const Appendable = @import("Appendable.zig");
+pub const Formatter = @import("Formatter.zig");
+pub const Client = @import("Client.zig");
+
+pub const parse = @import("parse.zig");
+
 const protocol = @import("protocol.zig");
-const Client = @import("Client.zig");
 const Core = @import("Core.zig");
 const Conn = @import("Conn.zig");
 const JS = @import("JetStream.zig");
-const Appendable = @import("Appendable.zig");
-const Formatter = @import("Formatter.zig");
 const pool = @import("messages.zig");
 const Subscriber = @import("Subscriber.zig");
 const Consumer = @import("Consumer.zig");
-const utils = @import("utils.zig");
+pub const utils = @import("utils.zig");
